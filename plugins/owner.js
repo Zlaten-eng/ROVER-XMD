981e78d99fe069f1507d820dbd712ac7d89b6722
Conway-eng <Mornah188@gmail.com>
2025-06-02 19:30:39 +0300

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -15,7 +15,7 @@ async (conn, mek, m, { from }) => {
 
         const vcard = 'BEGIN:VCARD\n' +
                       'VERSION:3.0\n' +
-                      `FN:Gotar Tech\n` +  
+                      `FN: Conway\n` +  
                       `TEL;type=CELL;type=VOICE;waid=${ownerNumber.replace('+', '')}:${ownerNumber}\n` + 
                       'END:VCARD';
 
