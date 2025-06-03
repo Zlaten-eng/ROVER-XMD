8cb5d9613013a4d7f20589ad79126312f6365222
Zlaten-eng <ronojepsat70@gmail.com>
2025-06-03 17:24:50 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-              // POWERED BY HACKLINK TECH.INC
+         // POWERED BY HACKLINK TECH.INC
 const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "ROVER-XMD~168VgbqR#SbqNE75tOswJtORI4Zdgoojgm24BeRKiVc5okCjnGTA",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
@@ -72,7 +72,7 @@ AUTO_TYPING: process.env.AUTO_TYPING || "false",
 // true for automatic show typing   
 READ_CMD: process.env.READ_CMD || "false",
 // true if want mark commands as read 
-DEV: process.env.DEV || "",
+DEV: process.env.DEV || "254788409105",
 //replace with your whatsapp number        
 ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
