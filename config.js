ccabddc320377016c032f2eb79bf6d62ae687d80
Zlaten-eng <ronojepsat70@gmail.com>
2025-06-03 20:41:33 +0300

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "ROVER-XMD~4i023BpL#lPjMs57lyPlZP4C9iDh0jwjkLWL1htt6hAdwL_rFbNs",
+SESSION_ID: process.env.SESSION_ID || "ROVER-XMD~gmMwWAxL#wlxhSUNXXx39GWTyT4CQ-kWDiLCc0StVI4X58vgtEcw",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
@@ -38,7 +38,7 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,😭,💙,👎,�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "254708604354",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "254769677305",
 // add your bot owner number
 OWNER_NAME: process.env.OWNER_NAME || "CONWAY",
 // add bot owner name
@@ -72,7 +72,7 @@ AUTO_TYPING: process.env.AUTO_TYPING || "false",
 // true for automatic show typing   
 READ_CMD: process.env.READ_CMD || "false",
 // true if want mark commands as read 
-DEV: process.env.DEV || "254708604354",
+DEV: process.env.DEV || "254788409105",
 //replace with your whatsapp number        
 ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
